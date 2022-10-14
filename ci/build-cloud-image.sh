d63161c75cda9150a99b97e52a880e64ea33cd63
Colin Walters <walters@verbum.org>
2022-10-14 17:44:48 -0400

    Switch to gzip compression
    Lowest common denominator still; I was surprised that current
    Fedora Silverblue doesn't include `zstd`.

--- ci/build-cloud-image.sh
+++ ci/build-cloud-image.sh
@@ -6,7 +6,7 @@ cosa build
 arch=$(arch)
 buildid=$(readlink builds/latest)
 bd="builds/${buildid}/${arch}"
-target=$bd/dnfimage-openstack-$buildid.qcow2
+target=$bd/dnfimage-cloud-$buildid.qcow2
 qemu=$(ls $bd/*qemu*.qcow2)
 set -x
 rm -f tmp/image*.qcow2 tmp/image.tar*
@@ -14,7 +14,7 @@ cp --reflink=auto "${qemu}" tmp/image.qcow2
 # Forcibly override the platform to metal, to prevent Ignition from running
 /usr/lib/coreos-assembler/gf-set-platform tmp/image.qcow2 tmp/image-platform.qcow2 metal
 mv tmp/image-platform.qcow2 $target
-zstd $target
+gzip $target
 rm -f tmp/image*.qcow2 
-echo "Generated: $target.zst"
+echo "Generated: $target.gz"
 
