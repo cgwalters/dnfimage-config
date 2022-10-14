895eef298af14451ad5929c435691804e74dc95f
Colin Walters <walters@verbum.org>
2022-10-14 16:35:07 -0400

    Switch to `compose image`

--- ci/build.sh
+++ ci/build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -xeuo pipefail
+target=$1
+shift
 src=$PWD
 cd /srv
 mkdir dnfimage
 cd dnfimage
 cosa init --transient $src
 cosa fetch
-cosa build ostree
+cosa build --prepare-only
+exec rpm-ostree compose image --format=registry --layer-repo tmp/repo src/config/manifest.yaml "${target}"
+
